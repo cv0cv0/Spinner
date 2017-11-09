12cebd7a66fb501c5a5d200069c9a82191077660
GR <linux2.0@foxmail.com>
2017-11-09 10:39:31 +0800

    Initial commit

---
+++ app/src/main/java/com/google/android/spinner/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.google.android.spinner
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val items = resources.getStringArray(R.array.language_list)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                Snackbar.make(view, items[position], Snackbar.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
